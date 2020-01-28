c22fe97f0fa2240cf96ad7925962b977f1437279
David Fellows <dfellows12@gmail.com>
2020-01-28 13:07:53 -0500

    Setup context provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Navbar from './components/Navbar'
 import BookList from './components/Booklist'
+import ThemeContextProvider from './contexts/ThemeContext'
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }
